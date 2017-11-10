e81bcabda5c1c639b2c26125a21239efebc2dda5
msgurgel <mgurgel5855@conestogac.on.ca>
2017-11-10 15:12:05 -0500

    Added version 0.1
    - Replies to tweets when @TweetGamesBot is mentioned
    - Rolls a 6-faced die through the /d6 command
    - Replies to replies

---
+++ tweetGames/collect.php
@@ -0,0 +1,182 @@
+<?php
+require_once('../lib/Phirehose.php');
+require_once('../lib/OauthPhirehose.php');
+
+
+/**
+ * Tweet Games Bot : Play games on Twitter (Tweet Collector)
+ *
+ * Code for this collector is based of fennb's example: ghetto-queue-collect.php
+ *
+ * Using libraries:
+ *    Phirehose by fennb - https://github.com/fennb/phirehose
+ *
+ * PHP version 7
+ *
+ * @author   Mateus Gurgel <mateus@comecodewith.me>
+ * @version  0.1
+ * @license  MIT
+ * @link     http://github.com/msgurgel/
+ * @see      Twitter-API-PHP, Phirehose
+ */
+class QueueCollector extends OauthPhirehose
+{
+
+  // Subclass specific constants
+  const QUEUE_FILE_PREFIX = 'phirehose-queue';
+  const QUEUE_FILE_ACTIVE = '.phirehose-queue.current';
+
+
+  // Member attributes specific to this subclass
+  protected $queueDir;
+  protected $rotateInterval;
+  protected $streamFile;
+  protected $statusStream;
+  protected $lastRotated;
+
+  /**
+   * Overidden constructor to take class-specific parameters
+   *
+   * @param string $username OAuth Access Token
+   * @param string $password OAuth Access Token Secret
+   * @param string $queueDir Directory where queue files are stored. Default = ./tmp
+   * @param integer $rotateInterval Time in sec that determines when to rotate current queue file. Default = 5
+   */
+  public function __construct($username, $password, $queueDir = './tmp', $rotateInterval = 5)
+  {
+    // Sanity check
+    if ($rotateInterval < 5)
+    {
+      throw new Exception('Rotate interval set too low - Must be >= 5 seconds');
+    }
+
+    // Set subclass parameters
+    $this->queueDir = $queueDir;
+    $this->rotateInterval = $rotateInterval;
+
+    // Call parent constructor
+    return parent::__construct($username, $password, Phirehose::METHOD_FILTER);
+  }
+
+  /**
+   * Enqueue each status
+   *
+   * @param string $status
+   */
+  public function enqueueStatus($status)
+  {
+
+    // Write the status to the stream (must be via getStream())
+    fputs($this->getStream(), $status .PHP_EOL);
+
+    /*
+     * As of version 0.1, we rotate the current queue file every time the Twitter
+     * Streaming API return a result. If the bot gets popular a some point in the
+     * future, we can use the code below to avoid overwhelming the server.
+     */
+
+     /*
+     $now = time();
+     if (($now - $this->lastRotated) > $this->rotateInterval)
+     {
+      // Mark last rotation time as now
+      $this->lastRotated = $now;
+
+      // Rotate it
+      $this->rotateStreamFile();
+    }
+    */
+
+    $this->rotateStreamFile();
+  }
+
+  /**
+   * Returns a stream resource for the current file being written/enqueued to
+   *
+   * @return resource
+   */
+  private function getStream()
+  {
+    // If we have a valid stream, return it
+    if (is_resource($this->statusStream))
+    {
+      return $this->statusStream;
+    }
+
+    // If it's not a valid resource, we need to create one
+    if (!is_dir($this->queueDir) || !is_writable($this->queueDir))
+    {
+      throw new Exception('Unable to write to queueDir: ' . $this->queueDir);
+    }
+
+    // Construct stream file name, log and open
+    $this->streamFile = $this->queueDir . '/' . self::QUEUE_FILE_ACTIVE;
+    $this->myLog('Opening new active status stream: ' . $this->streamFile);
+    $this->statusStream = fopen($this->streamFile, 'a'); // Append if present (crash recovery)
+
+    if (!is_resource($this->statusStream))
+    {
+      throw new Exception('Unable to open stream file for writing: ' . $this->streamFile);
+    }
+
+    // If we don't have a last rotated time, it's effectively now
+    // As of version 0.1, this really doesn't mean anything.
+    if ($this->lastRotated == NULL)
+    {
+      $this->lastRotated = time();
+    }
+
+    // return the resource
+    return $this->statusStream;
+  }
+
+  /**
+   * Rotates the stream file if due
+   */
+  private function rotateStreamFile()
+  {
+    // Close the stream
+    fclose($this->statusStream);
+
+    // Create queue file with timestamp so they're both unique and naturally ordered
+    $queueFile = $this->queueDir . '/' . self::QUEUE_FILE_PREFIX . '.' . date('Ymd-His') . '.queue';
+
+    // Do the rotate
+    rename($this->streamFile, $queueFile);
+
+    // Did it work?
+    if (!file_exists($queueFile))
+    {
+      throw new Exception('Failed to rotate queue file to: ' . $queueFile);
+    }
+
+    // At this point, all looking good - the next call to getStream() will create a new active file
+    $this->myLog('Successfully rotated active stream to queue file: ' . $queueFile);
+  }
+
+  /**
+   * Basic log function.
+   *
+   * @see error_log()
+   * @param string $messages
+   */
+  protected function myLog($message)
+  {
+    @error_log('collect.php: ' . $message, 0);
+  }
+
+} // End of class
+
+// The OAuth credentials from apps.twitter.com
+define("TWITTER_CONSUMER_KEY", "YOUR KEY");
+define("TWITTER_CONSUMER_SECRET", "YOUR SECRET");
+
+
+// The OAuth data for the twitter account
+define("OAUTH_TOKEN", "YOUR TOKEN");
+define("OAUTH_SECRET", "YOUR SECRET");
+
+// Start streaming/collecting
+$sc = new QueueCollector(OAUTH_TOKEN, OAUTH_SECRET);
+$sc->setTrack(array('TweetGamesBot'));
+$sc->consume();
---
+++ tweetGames/consume.php
@@ -0,0 +1,223 @@
+<?php
+
+/**
+ * Tweet Games Bot : Play games on Twitter (Tweet Reader and Replier)
+ *
+ * Using libraries:
+ *    Twitter-API-PHP by j7mbo  - https://github.com/J7mbo/twitter-api-php
+ *
+ * PHP version 7
+ *
+ * @author   Mateus Gurgel <mateus@comecodewith.me>
+ * @version  0.1
+ * @license  MIT
+ * @link     http://github.com/msgurgel/
+ * @see      Twitter-API-PHP
+ */
+
+require_once('../lib/TwitterAPIExchange.php');
+
+class QueueConsumer
+{
+
+  // Member attribs
+  protected $queueDir;
+  protected $filePattern;
+  protected $checkInterval;
+
+  // Authorization Keys from apps.twitter.com
+  private $settings = array(
+  'oauth_access_token' => "YOUR TOKEN",
+  'oauth_access_token_secret' => "YOUR SECRET",
+  'consumer_key' => "YOUR KEY",
+  'consumer_secret' => "YOUR SECRET"
+  );
+
+   // URL for posting tweets - check https://developer.twitter.com/en/docs for more
+   private $url = "https://api.twitter.com/1.1/statuses/update.json";
+   private $requestMethod = "POST";
+
+  /**
+   * Construct the consumer and start processing
+   */
+  public function __construct($queueDir = './tmp', $filePattern = 'phirehose-queue*.queue', $checkInterval = 5)
+  {
+    $this->queueDir = $queueDir;
+    $this->filePattern = $filePattern;
+    $this->checkInterval = $checkInterval;
+
+    // Sanity checks
+    if (!is_dir($queueDir))
+    {
+      throw new ErrorException('Invalid directory: ' . $queueDir);
+    }
+  }
+
+  /**
+   * Method that actually starts the processing task (never returns).
+   */
+  public function process()
+  {
+    // Init some things
+    $lastCheck = 0;
+    // Loop infinitely
+    while (TRUE)
+    {
+      // Get a list of queue files
+      $queueFiles = glob($this->queueDir . '/' . $this->filePattern);
+      $lastCheck = time();
+
+      $this->myLog('Found ' . count($queueFiles) . ' queue files to process...');
+
+      // Iterate over each file (if any)
+      foreach ($queueFiles as $queueFile)
+      {
+        $this->processQueueFile($queueFile);
+      }
+
+      // Wait until ready for next check
+      $this->myLog('Sleeping...');
+      while (time() - $lastCheck < $this->checkInterval)
+      {
+        sleep(1);
+      }
+    } // Infinite loop
+  } // End process()
+
+  /**
+   * Processes a queue file and does something with it (example only)
+   * @param string $queueFile The queue file
+   */
+  protected function processQueueFile($queueFile)
+  {
+    $this->myLog('Processing file: ' . $queueFile);
+
+    // Open file
+    $fp = fopen($queueFile, 'r');
+
+    // Check if something has gone wrong, or perhaps the file is just locked by another process
+    if (!is_resource($fp))
+    {
+      $this->myLog('WARN: Unable to open file or file already open: ' . $queueFile . ' - Skipping.');
+      return FALSE;
+    }
+
+    // Lock file
+    flock($fp, LOCK_EX);
+
+    // Loop over each line (1 line per status)
+    $statusCounter = 0;
+    while ($rawStatus = fgets($fp, 8192))
+    {
+      $statusCounter ++;
+
+      $data = json_decode($rawStatus, true);
+
+      if (is_array($data) && isset($data['user']['screen_name']) && $data['entities']['user_mentions'][0]['screen_name'] == 'TweetGamesBot')
+      {
+        $tweetFrom = $data['user']['screen_name'];    // Username of the user that mentioned the bot
+        $tweetID   = $data['id'];                     // ID of that tweet
+        $tweetText = urldecode($data['text']);        // The tweet itself
+        $isReply   = $data['in_reply_to_status_id'];  // ID of the tweet it is replying to
+
+        $this->myLog('Bot got mentioned: ' . $tweetFrom . ': ' . $tweetText . ' ***Extra info to follow***');
+        $this->myLog(var_export($data, true));
+
+        // Look for commands in tweet
+        $commands = strchr($tweetText, '/');
+        if (!$commands) // Did not find any '/' command
+        {
+          // Check if tweet is a reply or just a new mention
+          if ($isReply == NULL)
+          {
+            $arrPost = array('status' => '@' . $tweetFrom . ' Thanks for mentioning me! ' ."\u{1F60D}" . "\n\n" . 'Time: ' . date('h:i:s A'),
+                             'in_reply_to_status_id' => $tweetID
+                           );
+          }
+          else
+          {
+            $arrPost = array('status' => '@' . $tweetFrom . ' Thanks for replying to my tweet!' . "\n\n" . 'Time: ' . date('h:i:s A'),
+                             'in_reply_to_status_id' => $tweetID
+                           );
+          }
+        }
+        else // Found a '/' command
+        {
+          $commandArray = explode(" ", $commands);
+
+          $this->myLog('Found command in: ' .  $commands);
+
+          $v = var_export($commandArray, true);
+          $this->myLog('Var Export: ' . $v);
+
+          // Position 0 holds /command, other position holds possible arguments
+          switch ($commandArray[0])
+          {
+            // 6-faced Die
+            case '/d6':
+            $arrPost = array('status' => '@' . $tweetFrom . ' Roll the die! D6 Result: ' . rand(1,6) . "\n\n" . 'Time: ' . date('h:i:s A'),
+                             'in_reply_to_status_id' => $tweetID
+                           );
+              break;
+
+            // Unknown command
+            default:
+            $arrPost = array('status' => '@' . $tweetFrom . ' Unknown command: ' . $commandArray[0] . "\n\n" .  'Time: ' . date('h:i:s A'),
+                             'in_reply_to_status_id' => $tweetID
+                           );
+              break;
+          }
+        }
+
+        // Post response tweet
+        $this->postTweet($arrPost);
+        $this->myLog('Tweet: ' . $arrPost['status']);
+
+      }
+    } // End while
+
+    // Release lock and close
+    flock($fp, LOCK_UN);
+    fclose($fp);
+
+    // All done with this file
+    $this->myLog('Successfully processed ' . $statusCounter . ' tweets from ' . $queueFile . ' - deleting.');
+    unlink($queueFile);
+
+  }
+
+  /**
+   * Basic log function.
+   *
+   * @see error_log()
+   * @param string $messages
+   */
+  protected function myLog($message)
+  {
+    $myFile = fopen("logConsume.log", "a") or die("Unable to open file!");
+    $timeNow = date('c');
+    $txt = $timeNow . '--' . $message . "\r\n";
+    fwrite($myFile, $txt);
+    fclose($myFile);
+  }
+
+
+  /**
+  * Post tweet through the twitter-api-php library
+  *
+  * @see TwitterAPIExchange.php
+  * @param $postfields parameters for the Twitter API request
+  */
+  private function postTweet($postfields)
+  {
+    $twitter = new TwitterAPIExchange($this->settings);
+
+    $twitter->buildOauth($this->url, $this->requestMethod)
+    ->setPostfields($postfields)
+    ->performRequest();
+  }
+}
+
+// Construct consumer and start processing
+$gqc = new QueueConsumer();
+$gqc->process();

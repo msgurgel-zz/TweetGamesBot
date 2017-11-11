bcf06d03e4724bf77f8724799f6a3eb704571edc
msgurgel <mgurgel5855@conestogac.on.ca>
2017-11-11 16:55:49 -0500

    Added MySQL create database script

---
+++ tweetGames/createDatabase.sql
@@ -0,0 +1,11 @@
+/*
+*
+* File			: twGamesCreate.sql
+* By			: Mateus Gurgel (Matt)
+* Date			: November 11th, 2017
+* Description	: Creates the @TweetGamesBot database and its tables
+*
+*/
+
+CREATE DATABASE IF NOT EXISTS tweetgames;
+USE tweetgames;
